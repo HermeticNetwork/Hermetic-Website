2a87a5ecc3412de3e2a8c120e4c0224868fcd5ff
j316 <anon@j316.com>
2023-09-07 03:53:31 -0300

    ajustar +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script>
-    import SectionCards from "../components/SectionCards.svelte";
-    import Hero from "../components/Hero.svelte";
     import Navigation from "../components/Navigation.svelte";
-    
-    import "../style/global.css";
-    import "../style/reset.css";
+    import Hero from "../components/Hero.svelte";
+    import SectionCards from "../components/SectionCards.svelte";
     import Hr from "../components/Hr.svelte";
     import SectionNetwork from "../components/SectionNetwork.svelte";
     import Contribute from "../components/Contribute.svelte";
     import Footer from "../components/Footer.svelte";
+    
+    import "../style/global.css";
+    import "../style/reset.css";
 </script>
 
 <div id="page">
@@ -20,7 +20,3 @@
     <Contribute />
     <Footer />
 </div>
-
-<style>
-
-</style>
